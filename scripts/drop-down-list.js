a2862c8f6211d9bdbc232f21f73540105cfe98b9
NeYru07 <146803248+NeYru07@users.noreply.github.com>
2024-11-28 22:28:26 +0400

    Update 1

---
+++ scripts/drop-down-list.js
@@ -0,0 +1,44 @@
+document.querySelectorAll('.faq-question').forEach((question) => {
+	question.addEventListener('click', async () => {
+	  const icon = question.querySelector('.faq-icon');
+	  const answer = question.nextElementSibling;
+ 
+	  if (!answer.classList.contains('open')) {
+		 // Открытие: одновременно поворачиваем "+" и меняем на "-"
+		 await rotateAndChangeIcon(icon, '+', '-');
+	  } else {
+		 // Закрытие: меняем "-" обратно на "+"
+		 await rotateAndChangeIcon(icon, '-', '+');
+	  }
+ 
+	  // Раскрытие/скрытие текста
+	  answer.classList.toggle('open');
+	});
+ });
+ 
+ // Асинхронная функция для управления анимацией
+ async function rotateAndChangeIcon(icon, from, to) {
+	icon.style.transform = 'rotate(75deg)'; // Начинаем поворот
+	await new Promise((resolve) => setTimeout(resolve, 100)); // Ждём завершения анимации (300ms)
+	icon.textContent = to; // Меняем символ
+	icon.style.transform = 'rotate(0deg)'; // Возвращаем в нормальное положение
+ }
+ 
+//  Смена цен
+ document.getElementById('pricing-toggle-checkbox').addEventListener('change', function (event) {
+	const isAnnual = event.target.checked;
+ 
+	// Цены для ежемесячной и ежегодной подписок
+	const monthlyPrices = [100, 140, 210];
+	const annualPrices = [1000, 1400, 2100];
+	const month = ['/mo'];
+	const year = ['/ye']
+ 
+	// Обновляем цены в зависимости от состояния переключателя
+	document.querySelectorAll('.pricing-card').forEach((card, index) => {
+	  const priceElement = card.querySelector('.price-value');
+	  const periodElement = card.querySelectorAll('.period');
+	  priceElement.textContent = isAnnual ? `$${annualPrices[index]}` : `$${monthlyPrices[index]}`;
+	  periodElement.textContent = isAnnual ? `$${year[index]}` : `$${month[index]}`;
+	});
+ });
